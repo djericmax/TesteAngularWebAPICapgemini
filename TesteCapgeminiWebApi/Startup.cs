f0e0156a5aa1bfb73056846f2e4fd342ac3ded74
ERIC FIGUEIREDO <djericmax@hotmail.com>
2020-11-26 07:48:23 -0300

    Finalização da WebAPI.

--- TesteCapgeminiWebApi/Startup.cs
+++ TesteCapgeminiWebApi/Startup.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using TesteCapgeminiWebApi.Data;
+
 using TesteCapgeminiWebApi.Models;
 
 namespace TesteCapgeminiWebApi
